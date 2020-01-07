0f2d04db97e3321424bebd82ac3bdb1401e81fe5
Vinicyus Camopestrini <vinicyus.campes@gmail.com>
2020-01-07 13:45:03 -0300

    aula 25

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:Scaffold(
-        body: Container(),
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ButtonIcon(),
+              ButtonText(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 50,
+      color: Colors.red,
+      child: Icon(
+        Icons.add,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class ButtonText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 50,
+      color: Colors.blue,
+      alignment: Alignment.center,
+      child: Text(
+        "Botão",
+        style: TextStyle(color: Colors.white, fontSize: 18),
       ),
     );
   }
