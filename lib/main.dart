d9f8e6feceee40f88b0c852e7ebd96f7339ca3c4
Vinicyus Camopestrini <vinicyus.campes@gmail.com>
2020-01-07 13:09:49 -0300

    aula 24

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:HomeWidget(),
+      home:Scaffold(
+        body: Container(),
+      ),
     );
   }
 }
-
-class HomeWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child:BodyWidget() ,
-      
-    );
-  }
-}
-
-class BodyWidget extends StatefulWidget {
-  @override
-  _BodyWidgetState createState() => _BodyWidgetState();
-}
-
-class _BodyWidgetState extends State<BodyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        color: Colors.blue,),
-    );
-  }
-}
-
-//asasa
